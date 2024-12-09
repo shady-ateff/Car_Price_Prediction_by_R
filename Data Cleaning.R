1024630f6ca437d935899e7bd25e752fee1cf5a3
Shady Atef <shadysheko2003.ss@gmail.com>
2024-12-09 23:51:52 +0200

    Fix some Issues in data cleaning - remove nulls/dupliCates

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,13 +1,20 @@
 #Load data set
-data_frame <- read.csv("used_car_dataset.csv")
+data_frame <- read.csv("used_car_dataset.csv",na.strings="")
 
 #Checking data set structure and nulls
 str(data_frame)
 sum(is.na(data_frame))
 
+#Removing Nulls
+data_frame <- na.omit(data_frame)
+
+# Removing duplicates
+data_frame <- unique(data_frame)
+
 #Changing PostedDate column into date format for later analysis
 data_frame$PostedDate <- paste0(data_frame$PostedDate, "-15")
-data_frame$PostedDate <- as.Date(strptime(data_frame$PostedDate, format="%m-%y-%d"))
+Sys.setlocale("LC_TIME", "C")  # Set to English for date parsing"convert NOV -> 11"
+data_frame$PostedDate <- as.Date(strptime(data_frame$PostedDate, format="%b-%y-%d"))
 
 #Extracting year from PostedDate
 data_frame$PostedYear <- as.integer(format(data_frame$PostedDate, "%Y"))
@@ -16,8 +23,10 @@ data_frame$PostedYear <- as.integer(format(data_frame$PostedDate, "%Y"))
 unique(data_frame$Year + data_frame$Age == data_frame$PostedYear)
 data_frame$Age = (data_frame$PostedYear - data_frame$Year)
 
+
 #Checking nominal values
 unique(data_frame$Brand)
+unique(data_frame$kmDriven)
 unique(data_frame$model)
 unique(data_frame$Transmission)
 unique(data_frame$Owner)
@@ -32,13 +41,16 @@ data_frame$AskPrice = gsub(",", "", data_frame$AskPrice)
 data_frame$AskPrice = sub("₹", "", data_frame$AskPrice)
 data_frame$AskPrice = as.integer(data_frame$AskPrice)
 
+#Removing incorrect data
+data_frame <- data_frame[!(data_frame$kmDriven == 0 & data_frame$Age > 0 & data_frame$Owner == "second"),]
+
 #Fixing numeric data outliers problem
 boxplot(data_frame$kmDriven)
 kmDriven.Q1 <- quantile(data_frame$kmDriven, 0.25, na.rm = TRUE)
 kmDriven.Q3 <- quantile(data_frame$kmDriven, 0.75, na.rm = TRUE)
 kmDriven.IQR <- IQR(data_frame$kmDriven, na.rm = TRUE)
-data_frame$kmDriven[data_frame$kmDriven < kmDriven.Q1 - 1.5*kmDriven.IQR] = kmDriven.Q1 - 1.5*kmDriven.IQR
-data_frame$kmDriven[data_frame$kmDriven > kmDriven.Q3 + 1.5*kmDriven.IQR] = kmDriven.Q3 + 1.5*kmDriven.IQR
+data_frame$kmDriven[data_frame$kmDriven < kmDriven.Q1 - 1.5*kmDriven.IQR] <- kmDriven.Q1 - 1.5*kmDriven.IQR
+data_frame$kmDriven[data_frame$kmDriven > kmDriven.Q3 + 1.5*kmDriven.IQR] <- kmDriven.Q3 + 1.5*kmDriven.IQR
 
 boxplot(data_frame$AskPrice)
 AskPrice.Q1 <- quantile(data_frame$AskPrice, 0.25, na.rm = TRUE)
@@ -46,4 +58,3 @@ AskPrice.Q3 <- quantile(data_frame$AskPrice, 0.75, na.rm = TRUE)
 AskPrice.IQR <- IQR(data_frame$AskPrice, na.rm = TRUE)
 data_frame$AskPrice[data_frame$AskPrice < AskPrice.Q1 - 1.5*AskPrice.IQR] = AskPrice.Q1 - 1.5*AskPrice.IQR
 data_frame$AskPrice[data_frame$AskPrice > AskPrice.Q3 + 1.5*AskPrice.IQR] = AskPrice.Q3 + 1.5*AskPrice.IQR
-
