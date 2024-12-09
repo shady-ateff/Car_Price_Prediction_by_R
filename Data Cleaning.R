19921686884a9cf0cd42d3ff0c3c4812093bf4b7
Abdo-Zikry <abdulrahmanzikry@gmail.com>
2024-12-09 15:51:03 +0200

    Changed numeric strings into numbers

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -10,7 +10,7 @@ data_frame$PostedDate <- paste0(data_frame$PostedDate, "-15")
 data_frame$PostedDate <- as.Date(strptime(data_frame$PostedDate, format="%b-%y-%d"))
 
 #Extracting year from PostedDate
-data_frame$PostedYear <- as.numeric(format(data_frame$PostedDate, "%Y"))
+data_frame$PostedYear <- as.integer(format(data_frame$PostedDate, "%Y"))
 
 #Discovering and fixing Age inconsistency isssue
 unique(data_frame$Year + data_frame$Age == data_frame$PostedYear)
@@ -22,3 +22,12 @@ unique(data_frame$model)
 unique(data_frame$Transmission)
 unique(data_frame$Owner)
 unique(data_frame$FuelType)
+
+#Changing numeric strings into numeric
+data_frame$kmDriven = gsub(",", "", data_frame$kmDriven)
+data_frame$kmDriven = sub("km", "", data_frame$kmDriven)
+data_frame$kmDriven = as.numeric(data_frame$kmDriven)
+data_frame$AskPrice = gsub(",", "", data_frame$AskPrice)
+data_frame$AskPrice = sub("₹", "", data_frame$AskPrice)
+data_frame$AskPrice = as.numeric(data_frame$AskPrice)
+
