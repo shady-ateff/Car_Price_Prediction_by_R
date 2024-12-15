dcfcea41b5dca69fb6138f74b91fd7f59f7b54a6
Abdo-Zikry <abdulrahmanzikry@gmail.com>
2024-12-15 16:47:23 +0200

    Made lots of code edits

---
+++ Data Visualization.R
@@ -0,0 +1,44 @@
+#We visualize the numeric data in our data frame
+par(mfrow=c(1,3))
+boxplot(data_frame$kmDriven, main="Km Driven")
+boxplot(data_frame$Age, main="Age")
+boxplot(data_frame$AskPrice, main="Ask Price")
+
+#We then visualize our newly created features
+par(mfrow=c(1,3))
+boxplot(data_frame$kmPerYear, main="Km per Year")
+boxplot(data_frame$PostedMonths, main="Posted Months")
+
+#We now further inspect the non-normalized distributions
+par(mfrow=c(1,1))
+hist(data_frame$Age, main="Distribution of Age", col="lightgreen", xlab="Age (Years)")
+hist(data_frame$AskPrice, main="Distribution of Asking Price", col="lightcoral", xlab="Price")
+hist(data_frame$kmPerYear, main="km Driven Per Year", breaks=20,  col="lightpink", xlab="kmPerYear")
+
+#We now view categorial data distributions
+barplot(table(data_frame$Brand), las=2, col="steelblue", main="Count of Cars by Brand")
+par(mfrow=c(1,3))
+barplot(table(data_frame$FuelType), col="orange", main="Fuel Type Distribution")
+barplot(table(data_frame$Transmission), col="lightgreen", main="Transmission Type")
+barplot(table(data_frame$Owner), col="purple", main="Owner Type")
+
+#We then view some relations between numeric data
+par(mfrow=c(1,2))
+plot(data_frame$Age, data_frame$AskPrice, main="Price vs Age of Car", xlab="Age (Years)", ylab="Asking Price", col="blue", pch=16)
+plot(data_frame$kmDriven, data_frame$AskPrice, main="Price vs km Driven", xlab="kmDriven", ylab="Asking Price", col="red", pch=16)
+
+par(mfrow=c(1,2))
+plot(data_frame$Age, data_frame$AskPrice, main="Price vs Km per Year", xlab="Km per Year", ylab="Asking Price", col="green", pch=16)
+plot(data_frame$kmDriven, data_frame$AskPrice, main="Price vs Posted Months", xlab="Posted Months", ylab="Asking Price", col="orange", pch=16)
+
+#We now create the correlation matrix and view heat map
+encoded_data <- data_frame
+for (col in names(encoded_data)) if (!is.numeric(encoded_data[[col]])) encoded_data[[col]] <- as.numeric(as.factor(encoded_data[[col]]))
+encoded_data <- subset(encoded_data, select = -c(Year, PostedDate, AdditionInfo, PostedYear))
+cor_matrix <- cor(encoded_data, use="complete.obs")
+heatmap(cor_matrix, main="Correlation Heatmap", col=colorRampPalette(c("blue", "white", "red"))(100), scale="column")
+
+#We lastly create bar plot for correlation with ask price
+correlations <- sapply(encoded_data, function(col) cor(col, encoded_data$AskPrice, use="pairwise.complete.obs"))
+correlations <- abs(correlations[names(correlations) != "AskPrice"])
+barplot(correlations, main = "Absolute Correlation of Each Column with AskPrice",ylab = "Correlation Coefficient",col = "skyblue", las = 2, cex.names = 0.7)
---
+++ Feature Engineering.R
@@ -0,0 +1,32 @@
+#We diffrentiate between luxurious and regular brands
+luxury_brands <- c("BMW", "Mercedes-Benz", "Audi","Tesla","Porsche","Volvo","Lexus","Ferrari", "Lamborghini","Aston Martin","Bentley","Ford","Rolls-Royce" ,"Land Rover")
+data_frame$isLuxury <- data_frame$Brand %in% luxury_brands
+
+#We extract how many Km were driven per year
+data_frame$kmPerYear <- data_frame$kmDriven / data_frame$Age
+data_frame$kmPerYear[is.na(data_frame$kmPerYear)] <- 0
+data_frame$kmPerYear[is.infinite(data_frame$kmPerYear)] <- data_frame$kmDriven[is.infinite(data_frame$kmPerYear)]
+
+#We derive months since 1 month before earliest date from posted date
+#to provide useful integer data for models
+min(data_frame$PostedDate)
+data_frame$PostedMonths <- (as.integer(format(data_frame$PostedDate, "%Y")) - 2023)*12 + (as.integer(format(data_frame$PostedDate, "%m")) - 11)
+
+#We combine bith brand and model into 1 feature
+data_frame$Brand.model <-paste0(data_frame$Brand,"-",data_frame$model)
+
+
+#After Visualization Step:
+#We apply log transormation on km per Year and Ask Price
+encoded_data$LogkmPerYear = log(encoded_data$kmPerYear)
+encoded_data$LogAskPrice = log(encoded_data$AskPrice)
+
+#We now look at the correlation with log ask price
+encoded_data$kmPerYear <- NULL
+encoded_data$AskPrice <- NULL
+correlations <- sapply(encoded_data, function(col) cor(col, encoded_data$LogAskPrice, use="pairwise.complete.obs"))
+correlations <- abs(correlations[names(correlations) != "LogAskPrice"])
+barplot(correlations, main = "Absolute Correlation of Each Column with LogAskPrice",ylab = "Correlation Coefficient",col = "skyblue", las = 2, cex.names = 0.7)
+
+#We filter out features with low correlation with log ask price
+encoded_data <- subset(encoded_data, select = -c(Brand, model, PostedMonths, Brand.model))
